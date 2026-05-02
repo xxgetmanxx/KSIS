fcb69c10939d4bc578ff3066a9eaa5dd5dbdd651
Dmtr <xxgetmanxx@gmail.com>
2026-05-02 19:19:11 +0300

    40-40

---
+++ lab_5/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+const baseDir = "./storage_data"
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		path := filepath.Join(baseDir, filepath.Clean(r.URL.Path))
+
+		switch r.Method {
+
+		case http.MethodGet:
+			info, err := os.Stat(path)
+			if err != nil {
+				http.Error(w, "Not Found", 404)
+				return
+			}
+			if info.IsDir() {
+				entries, _ := os.ReadDir(path)
+				var list []string = []string{}
+				for _, e := range entries {
+					list = append(list, e.Name())
+				}
+				json.NewEncoder(w).Encode(list)
+				return
+			}
+			http.ServeFile(w, r, path)
+
+		case http.MethodPut:
+			os.MkdirAll(filepath.Dir(path), 0755)
+			if srcPath := r.Header.Get("X-Copy-From"); srcPath != "" {
+				src, _ := os.Open(filepath.Join(baseDir, filepath.Clean(srcPath)))
+				dst, _ := os.Create(path)
+				io.Copy(dst, src)
+				src.Close()
+				dst.Close()
+				log.Printf("[COPY] с %s в %s", srcPath, r.URL.Path)
+			} else {
+				f, _ := os.Create(path)
+				io.Copy(f, r.Body)
+				f.Close()
+				log.Printf("[SAVE] %s", r.URL.Path)
+			}
+			w.WriteHeader(201)
+
+		case http.MethodHead:
+			info, err := os.Stat(path)
+			if err != nil || info.IsDir() {
+				w.WriteHeader(404)
+				return
+			}
+			w.Header().Set("Content-Length", string(info.Size()))
+			w.WriteHeader(200)
+
+		case http.MethodDelete:
+			os.RemoveAll(path)
+			log.Printf("[DELETE] %s", r.URL.Path)
+			w.WriteHeader(204)
+
+		default:
+			w.WriteHeader(405)
+		}
+
+	})
+
+	log.Println("server_start")
+
+	http.ListenAndServe(":8080", nil)
+
+}
